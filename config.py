809fdbb354746cd90e419e4f89b80a0db2bed279
Bisma Humanyun <77541901+bismahumanyun@users.noreply.github.com>
2025-07-30 18:38:19 -0400

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-# config.py
-import os
-
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:gOnawazgO123@localhost/url_shortener'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'fallback-secret-key'
